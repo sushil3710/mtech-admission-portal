87dc3174686afa6fe3e3a1224c8ba4a9ad996ff6
Rohit Kinha <2020csb1118@iitrpr.ac.in>
2023-03-14 18:46:37 +0530

    first commit

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,7 @@ import App from "./App";
 import "./index.css";
 import "flowbite";
 import axios from "axios";
-axios.defaults.baseURL = 'http://localhost:8080/';
+axios.defaults.baseURL = 'https://mtech-admission-server.onrender.com/';
 
 ReactDOM.render(
   <React.StrictMode>
